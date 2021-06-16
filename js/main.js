c09f74af4d2115a67ec9530375c3ba74442fba67
MD Shimul <mdshemul480@gmail.com>
2021-06-16 17:13:06 +0600

    adding magnific to the js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ $(document).ready(() => {
 
     $buttons.click((event) => {
         $(".project-area .button-group button").removeClass("active");
-        console.log($(".project-area .button-group button"))
         event.target.classList.add("active")
 
         let selector = $(event.target).attr("data-filter");
@@ -14,5 +13,11 @@ $(document).ready(() => {
         return false;
     })
     $(".project-area .button-group #button1").trigger("click")
+
+
+    console.log($(".project-area .grid .test-popup-link"))
+    $(".project-area .grid .test-popup-link").magnificPopup({
+        type: 'image',
+    });
 }
 )
